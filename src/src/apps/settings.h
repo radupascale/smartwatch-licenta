7f8553481c27417a9c92ae5de9f2e9b3d30bfcdc
Radu <pascale.radu20@gmail.com>
2024-05-04 17:48:36 +0300

    feat: Setup wireless connection on boot

---
+++ src/src/apps/settings.h
@@ -0,0 +1,73 @@
+#pragma once
+#include "apps/app.h"
+
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
+#include <string.h>
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#define SSID "my_ssid"
+#define PASS "my_pass"
+#define MAXIMUM_RETRY 3
+
+/* The event group allows multiple bits for each event, but we only care
+about
+ * two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+
+class Settings : App
+{
+  private:
+	EventGroupHandle_t s_wifi_event_group;
+	int s_retry_num = 0;
+
+  public:
+	static Settings *instance;
+
+	Settings(Display *display) : App(display)
+	{
+		this->instance = this;
+	}
+
+    static void event_handler_static(void *arg, esp_event_base_t event_base,
+                                        int32_t event_id, void *event_data)
+    {
+        instance->event_handler(arg, event_base, event_id, event_data);
+    }
+
+	void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id,
+					   void *event_data);
+
+    /**
+     * @brief Init wifi using the wifi STATION example from ESP
+     * 
+     */
+    void wifi_init(void);
+
+    /**
+     * @brief Init the system clock using SNTP (wifi connection is required)
+     * 
+     */
+    void clock_init(void);
+
+	void setup_ui() override
+	{
+		return;
+	}
+	void update_ui() override
+	{
+		return;
+	}
+};
--- src/src/main.cpp
+++ src/src/main.cpp
@@ -4,6 +4,7 @@
 #include "components/ppg.h"
 
 #include "apps/watchface.h"
+#include "apps/settings.h"
 
 #include "core.h"
 #include "driver/gpio.h"
@@ -16,14 +17,23 @@
 #include "freertos/task.h"
 #include <stdio.h>
 
-static char const *TAG = "MAIN";
+static char const *MAIN_TAG = "MAIN";
 
 // init function pointer array
 int (*init_functions[])() = {bmi_init, drv_init, ppg_init, NULL};
 
+/**
+ * @brief Components
+ * 
+ */
 static Display display;
 
+/**
+ * @brief Applications
+ * 
+ */
 static WatchFace *watch_face = nullptr;
+static Settings *settings = nullptr;
 
 void os_init()
 {
@@ -31,27 +41,35 @@ void os_init()
 	int i;
 
     status = display.init();
-    if (status < 0) 
+    if (status < 0)
     {
-        ESP_LOGE(TAG, "Failed to initialize display.");
+        ESP_LOGE(MAIN_TAG, "Failed to initialize display.");
     }
 
+	/* TODO: init SD */
 	for (i = 0; init_functions[i] != NULL; i++) {
 		status = init_functions[i]();
 		if (status < 0) {
-			ESP_LOGE(TAG, "Failed to initialize module %d.", i);
+			ESP_LOGE(MAIN_TAG, "Failed to initialize module %d.", i);
 		}
 	}
 
-	/* TODO: init SD */
+    /* TODO: Use NVS for persistent wifi configs */
+	esp_err_t ret = nvs_flash_init(); if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+		ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
 
     /* Initialize applications */
+    settings = new Settings(&display);
+    settings->wifi_init();
+
     watch_face = new WatchFace(&display);
     watch_face->setup_ui();
 
-	/* TODO: other things to initialize (timers, buttons, wifi, bluetooth) */
-
-	ESP_LOGI(TAG, "Finish modules initialization.");
+	ESP_LOGI(MAIN_TAG, "Finish modules initialization.");
 }
 
 void os_update_display(void *pvParameter)
@@ -59,9 +77,11 @@ void os_update_display(void *pvParameter)
 	while (1) {
 		vTaskDelay(pdMS_TO_TICKS(10));
 
+        watch_face->update_ui();
         display.render();
 	}
 }
+
 
 extern "C" {
 void app_main()
