8e6810f53eba3450fa7444cf0171e38c0c28654e
Radu <pascale.radu20@gmail.com>
2024-06-03 13:45:38 +0300

    feat: Add button and SPI pins defines

--- src/include/configs/core.h
+++ src/include/configs/core.h
@@ -1,8 +1,19 @@
 #pragma once
+#include "driver/adc.h"
+#include "driver/gpio.h"
 
+#define ADC_BAT ADC1_CHANNEL_4
+#define CHG_STAT GPIO_NUM_15
+#define BUTTON_SELECT GPIO_NUM_0
+#define BUTTON_UP GPIO_NUM_11
+#define BUTTON_DOWN GPIO_NUM_26
 
-#define BUTTON_SELECT 26
-#define BUTTON_UP 0
-#define BUTTON_DOWN 11
+#define MISO 37
+#define SCK 36
+#define MOSI 35
+#define SS_SD 3
+#define SS_TFT 17
+
+#define TEST_IMU 0
 
 void os_init();
