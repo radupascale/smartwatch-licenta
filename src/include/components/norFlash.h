1119f057cc1437c5db6ef915896db9e9111eb2e6
Radu <pascale.radu20@gmail.com>
2024-06-07 14:33:54 +0300

    feat: Add untested simple flash implementation

---
+++ src/include/components/norFlash.h
@@ -0,0 +1,80 @@
+#pragma once
+#include "SPI.h"
+#include "components/component.h"
+#include "configs/core.h"
+
+#define SPI_BYTE_ORDER_MSB 1
+
+#define FLASH_PAGE_SIZE (256)
+/**
+ * @brief SPI instruction codes from the datasheet:
+ * https://www.winbond.com/resource-files/W25Q256JW%20SPI%20RevJ%2003102021%20Plus.pdf
+ *
+ */
+
+#define flash_instr_t uint8_t
+#define READ_SRG1 (0x05)
+#define WR_ENABLE (0x06) // set before any PAGE_PROGRAM, ERASE, or WRITE to SR
+#define WR_DISABLE                                                             \
+	(0x04) // automatically set after above (PAGE_PROGRAM, ERASE, WRITE to SR)
+#define READ_DATA (0x03)
+#define PAGE_PROGRAM (0x02) // write data?
+#define SECTOR_ERASE (0x20) // sector = 4KB
+#define BLOCK_ERASE_32KB (0x52)
+#define BLOCK_ERASE_64KB (0x52)
+#define CHIP_ERASE (0x60)
+#define RST_EN (0x66)
+#define RST (0x99)
+
+#define BUSY_BIT 0
+
+#define DUMMY_VAL (0x00)
+#define MASK (0xff)
+
+class NORFlash : Component
+{
+  private:
+	SPISettings *busSettings = nullptr;
+
+  public:
+	/**
+	 * @brief This uses the default SPI bus in SPI.cpp
+	 * By default, this flash uses 3 byte addresses (set by
+	 * ADP = 0 in one of the status registers)
+	 *
+	 * @return esp_err_t
+	 */
+	esp_err_t init(void) override;
+	NORFlash();
+	~NORFlash();
+	esp_err_t configure_ss_pin(void);
+	void select(void);
+	void deselect(void);
+
+	void send_instr(flash_instr_t instr);
+
+	/* SPI operations for W25Q256 module */
+
+	void chip_reset();
+	void read_status_register(flash_instr_t instr, uint8_t *data);
+	/**
+	 * @brief BUSY bit has to be low before any transactions
+	 *
+	 */
+	void wait_busy();
+	void write_enable();
+	void read_data(uint32_t addr, uint8_t *buff, size_t size);
+	/**
+	 * @brief Implements the page_program instruction from the datasheet
+	 * write_enable -> send_address -> send_data -> end
+	 * NOTE: If an entire 256 byte page is to be programmed, the last address
+	 * byte (the 8 least significant address bits) should be set to 0.
+     * This is why this function returns an error if the given size
+     * would overwrite data from the current page
+	 * @param addr
+	 * @param data
+	 * @param size
+	 */
+	esp_err_t page_program(uint32_t addr, uint8_t *data, size_t size);
+	void chip_erase();
+};
---
+++ src/src/components/norFlash.cpp
@@ -0,0 +1,153 @@
+#include "components/norFlash.h"
+#include "esp_log.h"
+
+static const char *TAG = "NORFlash";
+
+NORFlash::NORFlash()
+{
+}
+NORFlash::~NORFlash()
+{
+	if (busSettings)
+		delete busSettings;
+}
+
+esp_err_t NORFlash::init(void)
+{
+	esp_err_t ret = ESP_OK;
+
+	ret = configure_ss_pin();
+	if (ret != ESP_OK) {
+		ESP_LOGI(TAG, "Failed to configure SS pin");
+		return ret;
+	}
+	SPI.begin(SCK, MISO, MOSI, SS_FLASH);
+	busSettings = new SPISettings(SPI_SPEED, SPI_BYTE_ORDER_MSB, SPI_MODE0);
+
+	return ret;
+}
+
+esp_err_t NORFlash::configure_ss_pin()
+{
+	gpio_config_t io_conf;
+	esp_err_t err;
+
+	io_conf.intr_type = GPIO_INTR_DISABLE;
+	io_conf.mode = GPIO_MODE_OUTPUT;
+	io_conf.pin_bit_mask = (1ULL << SS_FLASH);
+	io_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
+	err = gpio_config(&io_conf);
+
+	if (err != ESP_OK) {
+		ESP_LOGI(TAG, "Failed to configure SS pin");
+		return err;
+	}
+
+	return err;
+}
+
+void NORFlash::select(void)
+{
+	gpio_set_level(SS_FLASH, 0);
+}
+
+void NORFlash::deselect(void)
+{
+	gpio_set_level(SS_FLASH, 1);
+}
+
+void NORFlash::send_instr(flash_instr_t instr)
+{
+	SPI.beginTransaction(*busSettings);
+	select();
+	SPI.transfer(instr);
+	deselect();
+	SPI.endTransaction();
+}
+
+void NORFlash::chip_reset()
+{
+	send_instr(RST_EN);
+	send_instr(RST);
+}
+
+void NORFlash::wait_busy()
+{
+	uint8_t sr;
+
+	read_status_register(READ_SRG1, &sr);
+	while (sr & (1 << BUSY_BIT)) {
+		read_status_register(READ_SRG1, &sr);
+	}
+}
+
+void NORFlash::write_enable()
+{
+    select();
+	send_instr(WR_ENABLE);
+    deselect();
+}
+void NORFlash::read_status_register(flash_instr_t instr, uint8_t *data)
+{
+	SPI.beginTransaction(*busSettings);
+	select();
+	send_instr(READ_SRG1);
+	*data = SPI.transfer(DUMMY_VAL);
+	deselect();
+	SPI.endTransaction();
+}
+
+void NORFlash::chip_erase()
+{
+	write_enable();
+	send_instr(CHIP_ERASE);
+	wait_busy();
+}
+
+void NORFlash::read_data(uint32_t addr, uint8_t *buff, size_t size)
+{
+	uint32_t i;
+
+	/**
+	 * @brief send instruction + address
+	 * then wait for SIZE bytes of data
+	 *
+	 */
+	SPI.beginTransaction(*busSettings);
+	select();
+	SPI.transfer(READ_DATA);
+	SPI.transfer((addr >> 16) & MASK);
+	SPI.transfer((addr >> 8) & MASK);
+	SPI.transfer(addr & MASK);
+	for (i = 0; i < size; i++) {
+		buff[i] = SPI.transfer(DUMMY_VAL);
+	}
+	deselect();
+	SPI.endTransaction();
+}
+
+esp_err_t NORFlash::page_program(uint32_t addr, uint8_t *data, size_t size)
+{
+	uint32_t i;
+
+	if (size > (FLASH_PAGE_SIZE - (addr & 0xff)))
+		return ESP_ERR_INVALID_SIZE;
+
+	SPI.beginTransaction(*busSettings);
+	select();
+	SPI.transfer(WR_ENABLE);
+	deselect();
+    select();
+	SPI.transfer(PAGE_PROGRAM);
+	SPI.transfer((addr >> 16) & MASK);
+	SPI.transfer((addr >> 8) & MASK);
+	SPI.transfer(addr & MASK);
+	for (i = 0; i < size; i++) {
+		SPI.transfer(data[i]);
+        wait_busy();
+	}
+	deselect();
+	SPI.endTransaction();
+
+    return ESP_OK;
+}
